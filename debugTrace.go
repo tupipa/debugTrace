0ac378e9e1270493c5c8c1d29b9e0918bce269d8
lelema-srv <lelema.zh@gmail.com>
2017-03-23 11:14:22 -0400

    add string para to Trace

--- debugTrace.go
+++ debugTrace.go
@@ -5,14 +5,12 @@ import (
 	"runtime"
 )
 
-func Trace() {
+// Trace will print the current function name and the code line number
+func Trace(info string) {
 
-	pc := make ([] uintptr, 10)
-	runtime.Callers (2,pc)
+	pc := make([]uintptr, 10)
+	runtime.Callers(2, pc)
 	f := runtime.FuncForPC(pc[0])
 	file, line := f.FileLine(pc[0])
-	fmt.Printf("Lele: %s:%d %s\n", file, line, f.Name())
+	fmt.Printf("Lele: %s, %s:%d %s\n", info, file, line, f.Name())
 }
-
-
-
